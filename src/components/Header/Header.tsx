be84760ec9cdfd87fa50fad85a4a70b0bb2171c0
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-09-13 13:56:04 +0300

    added Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,25 @@
+import { NavLink } from 'react-router-dom'
+
 import s from './Header.module.scss'
+import { activeLink } from './Header.types'
+
+const setActive = ({ isActive }: activeLink) =>
+  isActive ? `${s.navLink} ${s.activeLink}` : `${s.navLink}`
 
 export const Header = () => {
-  return <div className={s.header}>Header</div>
+  return (
+    <header className={s.header}>
+      <div>Logo</div>
+
+      <NavLink to='/' className={setActive}>
+        Home
+      </NavLink>
+      <NavLink to='drivers' className={setActive}>
+        Drivers
+      </NavLink>
+      <NavLink to='favorites' className={setActive}>
+        Favorites
+      </NavLink>
+    </header>
+  )
 }
