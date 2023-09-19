a034e00783882ac5c1ba9e6cffd5e91a03dfb870
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-09-19 19:26:23 +0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_KEY: string
+  readonly VITE_PROJECT_ID: string
+  readonly VITE_DATABASE_NAME: string
+  readonly VITE_SENDER_ID: string
+  readonly VITE_APP_ID: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
