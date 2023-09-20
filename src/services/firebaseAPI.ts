b903500ae52a1b5f9a75c1f7ec16ecc684cc50dd
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-09-20 17:12:02 +0300

    creates get drivers api for pagination

--- src/services/firebaseAPI.ts
+++ src/services/firebaseAPI.ts
@@ -1,11 +1,40 @@
-import { getDatabase, ref, onValue } from 'firebase/database'
+import {
+  getDatabase,
+  ref,
+  onValue,
+  query,
+  orderByChild,
+  limitToFirst,
+  startAt,
+  endAt,
+} from 'firebase/database'
 
 import { db } from './firebaseConfig'
 
-export const getTotalDriversApi = async () => {
-  const database = getDatabase(db)
-  onValue(ref(database, '/drivers'), (snapshot) => {
-    const data = snapshot.val() || {}
+const database = getDatabase(db)
+const dbRef = ref(database, '/drivers')
+
+const getTotalDriversApi = () => {
+  onValue(dbRef, (snap) => {
+    const data = snap.val() || []
+    console.log('🚀 ~ getTotalDriversApi ~ data:', data)
+
     return data
   })
 }
+
+const getDriversApi = () => {
+  onValue(
+    query(dbRef, orderByChild('driverId'), limitToFirst(4), startAt(1201)),
+    // query(dbRef, orderByChild('driverId'), startAt(1201), endAt(1204)),
+    // query(dbRef, orderByChild('driverId'), startAt(1204), endAt(1207)),
+    (snap) => {
+      const data = snap.val() || []
+      console.log('🚀 ~ getDriversApi ~ data:', data)
+
+      return data
+    },
+  )
+}
+
+export { getTotalDriversApi, getDriversApi }
