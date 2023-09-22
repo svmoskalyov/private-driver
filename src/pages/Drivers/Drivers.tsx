b6163cd5913c3cfb53e594dd8a46429a7e6bd829
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-09-22 20:22:49 +0300

    Update Drivers.tsx

--- src/pages/Drivers/Drivers.tsx
+++ src/pages/Drivers/Drivers.tsx
@@ -1,7 +1,13 @@
 import s from './Drivers.module.scss'
 
+import { getTotalDriversApi } from '../../services/firebaseAPI'
+
 const Drivers = () => {
-  return <div className={s.drivers}>Drivers</div>
+  return (
+    <div className={s.drivers}>
+      <button onClick={() => getTotalDriversApi()}>total drivers </button>
+    </div>
+  )
 }
 
 export default Drivers
