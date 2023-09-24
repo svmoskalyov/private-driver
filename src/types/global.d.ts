89783c914bc7d522a8500fbbf2837326316c76cf
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-09-24 21:14:06 +0300

    Update global.d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -14,5 +14,21 @@ declare global {
   type Driver = {
     driverId: number
     name: string
+    surname: string
+    languages: string[]
+    categories: string[]
+    rating: number
+    reviews: object[]
+    reviews: {
+      reviewer_name: string
+      reviewer_rating: number
+      comment: string
+    }[]
+    price_per_hour: number
+    trips_made: number
+    avatar_url: string
+    driver_info: string
+    skills: string[]
+    experience: string
   }
 }
