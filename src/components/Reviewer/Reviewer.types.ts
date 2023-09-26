1fe8faaa9771a5ec3e490f941e48fd3de2e7766c
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-09-26 22:26:18 +0300

    Update Reviewer.types.ts

--- src/components/Reviewer/Reviewer.types.ts
+++ src/components/Reviewer/Reviewer.types.ts
@@ -1,10 +1,1 @@
-// export type Props = {
-//   reviews: {
-//     reviewer_name: string
-//     reviewer_rating: number
-//     reviewer_avatar: string
-//     comment: string
-//   }[]
-// }
-
 export type Props = Pick<Driver, 'reviews'>
