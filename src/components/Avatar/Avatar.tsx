a3e9ff7b66a660acd6a5fc0a25018f0c74dacba1
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-09-27 23:36:17 +0300

    added Avatar

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,15 +1,25 @@
 import s from './Avatar.module.scss'
 import { Props } from './Avatar.types'
 
-export const Avatar = ({ src, name }: Props) => (
-  <img
-    className={
-      name === 'reviewer' ? `${s.avatar} ${s.avatarReviewer}` : s.avatar
-    }
-    loading='lazy'
-    width={name === 'reviewer' ? 44 : 96}
-    height={name === 'reviewer' ? 44 : 96}
-    src={src}
-    alt={name === 'reviewer' ? 'avatar reviewer' : 'avatar driver'}
-  />
-)
+export const Avatar = ({ src, name }: Props) =>
+  name === 'reviewer' ? (
+    <img
+      className={`${s.avatar} ${s.avatarReviewer}`}
+      loading='lazy'
+      width={44}
+      height={44}
+      src={src}
+      alt={'avatar reviewer'}
+    />
+  ) : (
+    <div className={s.avatarDriverWrapper}>
+      <img
+        className={s.avatar}
+        loading='lazy'
+        width={96}
+        height={96}
+        src={src}
+        alt={'avatar driver'}
+      />
+    </div>
+  )
