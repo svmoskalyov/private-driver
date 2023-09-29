7906ba2b75d74c3f9290f8d0ac3f6a9f71fd7042
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-09-29 21:56:09 +0300

    create component Heart

--- src/components/Heart/Heart.tsx
+++ src/components/Heart/Heart.tsx
@@ -1,15 +1,19 @@
+import { FiHeart } from 'react-icons/fi'
+
 import s from './Heart.module.scss'
 import { Props } from './Heart.types'
 
 export const Heart = ({ isFav }: Props) => {
   return (
-    <label className={s.heart}>
+    <label className={s.chekboxHeart}>
       <input
+        className={'visuallyHidden'}
         type='checkbox'
         name='status'
         checked={isFav}
         onChange={() => console.log('checked')}
       />
+      <FiHeart className={s.heart} />
     </label>
   )
 }
