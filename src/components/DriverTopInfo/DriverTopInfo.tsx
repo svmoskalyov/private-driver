2c43f12d895d2d9322fd481911a0c0ee8871a352
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-09-29 21:18:05 +0300

    create component DriverTopInfo

--- src/components/DriverTopInfo/DriverTopInfo.tsx
+++ src/components/DriverTopInfo/DriverTopInfo.tsx
@@ -1,19 +1,21 @@
 import s from './DriverTopInfo.module.scss'
 import { Props } from './DriverTopInfo.types'
 
+import { Rating } from '../Rating'
+
 export const DriverTopInfo = ({
   rating,
   price_per_hour,
   trips_made,
 }: Props) => (
-  <div className={s.driverTopInfo}>
-    <ul className={s.topInfoList}>
-      <li className={s.topInfoItem}>Trips made: {trips_made}</li>
-      <li className={s.topInfoItem}>Rating {rating}</li>
-      <li className={s.topInfoItem}>
-        Price / 1 hour:
-        <span className={s.topInfoPrice}>{price_per_hour}$</span>
-      </li>
-    </ul>
-  </div>
+  <ul className={s.topInfoList}>
+    <li className={s.topInfoItem}>Trips made: {trips_made}</li>
+    <li className={s.topInfoItem}>
+      <Rating text='Rating' value={rating} />
+    </li>
+    <li className={s.topInfoItem}>
+      Price / 1 hour:
+      <span className={s.topInfoPrice}>{price_per_hour}$</span>
+    </li>
+  </ul>
 )
