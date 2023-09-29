a5bc7385ea49fe95cc1fdfbe31b8e6d371fdc940
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-09-29 18:13:04 +0300

    create component DriverCategories

--- src/components/DriverCategories/DriverCategories.tsx
+++ src/components/DriverCategories/DriverCategories.tsx
@@ -2,15 +2,22 @@ import s from './DriverCategories.module.scss'
 import { Props } from './DriverCategories.types'
 
 export const DriverCategories = ({ categories }: Props) => {
+  const isCoiced = false
+
   return (
-    <div className={s.driverCategories}>
-      <ul className={s.categoriesList}>
-        {categories?.map((e, i) => (
-          <li className={s.categoriesItems} key={i}>
-            {e}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className={s.categoriesList}>
+      {categories?.map((e, i) => (
+        <li
+          className={
+            isCoiced
+              ? `${s.categoriesItems} ${s.categoriesItemsChoice}`
+              : s.categoriesItems
+          }
+          key={i}
+        >
+          {e}
+        </li>
+      ))}
+    </ul>
   )
 }
