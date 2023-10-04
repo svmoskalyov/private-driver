392049df7b30de96a47d205f9647bdefc49aa24c
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-10-04 22:52:50 +0300

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,16 @@
 import s from './Home.module.scss'
 
 import { FormBook } from '../../components/FormBook'
+import { FormLogin } from '../../components/FormLogin'
+import { FormRegistration } from '../../components/FormRegistration'
 
 const Home = () => {
   return (
     <div className={s.home}>
       Home
       <FormBook />
+      <FormRegistration />
+      <FormLogin />
     </div>
   )
 }
