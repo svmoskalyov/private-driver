262801532bfa2d33ff60916587075781a3b236fc
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-10-06 12:15:40 +0300

    Update yupSchemas.ts

--- src/utils/yupSchemas.ts
+++ src/utils/yupSchemas.ts
@@ -13,10 +13,11 @@ export const loginSchema = yup.object().shape({
     .required('Email is a required field'),
   password: yup
     .string()
-    .matches(
-      /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
-      'Password must contain at least 8 characters, one uppercase, one number and one special case character',
-    )
+    .min(8, 'Password must be 8 characters long')
+    .matches(/[0-9]/, 'Password requires a number')
+    .matches(/[a-z]/, 'Password requires a lowercase letter')
+    .matches(/[A-Z]/, 'Password requires an uppercase letter')
+    .matches(/[^\w]/, 'Password requires a symbol')
     .required('Password is a required field'),
 })
 
