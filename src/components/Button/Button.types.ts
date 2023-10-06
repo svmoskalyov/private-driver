f3d679b35772480751259fb491c7c61a0342f405
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-10-06 11:36:30 +0300

    Update Button.types.ts

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -3,7 +3,7 @@ import { MouseEventHandler, ReactNode } from 'react'
 export interface Props {
   className?: string | undefined
   type?: 'button' | 'submit' | 'reset'
-  onClick: MouseEventHandler
+  onClick?: MouseEventHandler
   children: ReactNode
   allyProps?: { [x: string]: unknown }
 }
