f76f2f8497108fc3d69d23e81558618e0a1aefbd
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-10-07 21:21:54 +0300

    Update Favorites.tsx

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,7 +1,27 @@
 import s from './Favorites.module.scss'
 
+import { CardList } from '../../components/CardList'
+import { useAppSelector } from '../../hooks/reduxHooks'
+import {
+  selectError,
+  selectFavorites,
+  selectIsLoading,
+} from '../../redux/drivers/driversSelectors'
+
 const Favorites = () => {
-  return <div className={s.favorites}>Favorites</div>
+  const loading = useAppSelector<boolean>(selectIsLoading)
+  const err = useAppSelector<string | null>(selectError)
+  const favorites = useAppSelector<DriverFav[]>(selectFavorites)
+
+  return (
+    <>
+      {favorites.length !== 0 && <CardList catalog={favorites} />}
+
+      {favorites.length === 0 && (
+        <div className={s.notFavCar}>No favorites drivers </div>
+      )}
+    </>
+  )
 }
 
 export default Favorites
