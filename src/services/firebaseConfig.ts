92a0becf1be60193dbd0a0540b6321ff0a0eed18
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-10-19 22:42:49 +0300

    Update firebaseConfig.ts

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -10,4 +11,5 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_APP_ID,
 }
 
-export const db = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
