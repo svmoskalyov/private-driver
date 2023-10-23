e94e282a1f6ca17adbb85812ba5bbf7d96dc8c48
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-10-23 21:23:23 +0300

    Update Heart.tsx

--- src/components/Heart/Heart.tsx
+++ src/components/Heart/Heart.tsx
@@ -1,18 +1,13 @@
 import { FiHeart } from 'react-icons/fi'
 
-import { useNavigate } from 'react-router-dom'
-
 import s from './Heart.module.scss'
 
-import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks'
-import { selectIsAuth } from '../../redux/auth/authSelectors'
+import { useAppDispatch } from '../../hooks/reduxHooks'
+
 import { setFavorite } from '../../redux/drivers/driversSlice'
 
 export const Heart = (el: DriverFav) => {
-  const navigate = useNavigate()
-
   const dispatch = useAppDispatch()
-  const isAuth = useAppSelector(selectIsAuth)
   const { isFav } = el
 
   return (
@@ -22,11 +17,6 @@ export const Heart = (el: DriverFav) => {
         type='checkbox'
         name='status'
         checked={isFav}
-        // onChange={
-        //   isAuth
-        //     ? () => dispatch(setFavorite({ ...el, isFav: !isFav }))
-        //     : () => navigate('/login')
-        // }
         onChange={() => dispatch(setFavorite({ ...el, isFav: !isFav }))}
       />
       <FiHeart className={s.heart} />
