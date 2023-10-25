c681f973ace08976cf0a94414488831ed027d9e5
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-10-25 19:24:29 +0300

    Update Button.types.ts

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -4,6 +4,7 @@ export interface Props {
   className?: string | undefined
   type?: 'button' | 'submit' | 'reset'
   onClick?: MouseEventHandler
+  disabled?: boolean
   children: ReactNode
   allyProps?: { [x: string]: unknown }
 }
