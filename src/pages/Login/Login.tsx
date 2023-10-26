557797c6d080a19bdbbedce9167060295aaa339e
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-10-26 10:08:32 +0300

    Update Login.tsx

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,23 +1,41 @@
-import { useCallback, useState } from 'react'
-
+import { useCallback, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 import { FormLogin } from '../../components/FormLogin'
 import { Modal } from '../../components/Modal'
+import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks'
+import { selectAuthError } from '../../redux/auth/authSelectors'
+import { setErrNull } from '../../redux/auth/authSlice'
 
 const Login = () => {
   const navigate = useNavigate()
+  const dispatch = useAppDispatch()
+  const error = useAppSelector(selectAuthError)
+  const [showModal, setShowModal] = useState<boolean>(true)
 
   const handleCloseModal = useCallback(() => {
     navigate('/')
   }, [navigate])
 
-  const [showModal, setShowModal] = useState<boolean>(true)
-
   const toggleModal = () => {
     setShowModal(!showModal)
     handleCloseModal()
+    dispatch(setErrNull())
   }
+
+  const createErrorMessage = (error: string) => {
+    if (`${error}` === 'auth/invalid-login-credentials') {
+      toast.error('Email or password is not correct')
+    }
+    return error
+  }
+
+  useEffect(() => {
+    if (error) {
+      createErrorMessage(error)
+    }
+  }, [error])
 
   return (
     <>
