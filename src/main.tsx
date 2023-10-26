97b83e23545e4d8a0eba0bf892f914c439ddf9ca
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-10-26 10:08:10 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
@@ -11,11 +10,9 @@ const rootElement = document.getElementById('root')!
 const root = createRoot(rootElement as HTMLElement)
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
 )
