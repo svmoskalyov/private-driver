dc0ec1878064402fca147abb4c6ea7165c810bb6
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-10-28 12:01:41 +0300

    added ArrowStart to home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,40 @@
+import { Link } from 'react-router-dom'
+
 import s from './Home.module.scss'
 
-const Home = () => {
-  return <div className={s.home}>Home</div>
-}
+import driver from '../../assets/images/driver.jpg'
+import { ArrowStart } from '../../components/ArrowStart'
+
+const Home = () => (
+  <>
+    <ul className={s.home}>
+      <li>
+        <h1 className={s.title}>
+          Spend the time of the trip in comfort,
+          <mark className={s.titleAccent}>choose your driver</mark>
+        </h1>
+        <Link to='drivers' className={s.getStarted}>
+          <ArrowStart name='Get started' />
+        </Link>
+      </li>
+      <li>
+        <p className={s.text}>
+          Travel in comfort, relax or solve your business on the road without
+          thinking about driving rules, contact our professional drivers.
+        </p>
+      </li>
+      <li>
+        <img
+          className={s.img}
+          loading='lazy'
+          src={driver}
+          width={600}
+          height={375}
+          alt='image driver'
+        />
+      </li>
+    </ul>
+  </>
+)
 
 export default Home
