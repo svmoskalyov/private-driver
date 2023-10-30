11326e20fc71335844e9be9b7e4b6ed73e9a75df
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-10-30 13:31:48 +0200

    added responsive

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -5,6 +5,6 @@ import logo from '../../assets/icons/speedometer.ico'
 export const Logo = () => (
   <div className={s.logo}>
     <img src={logo} width={28} height={28} alt='logo' />
-    PD
+    Private Driver
   </div>
 )
